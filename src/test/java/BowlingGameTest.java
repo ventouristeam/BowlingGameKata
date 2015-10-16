5e75352acf3f79172a1dbc6580a172b2aecd2f2d
rRob Swartenbroekx <rob.swartenbroekx@gmail.com>
2015-10-16 10:46:24 +0200

    setup project

---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,12 @@
+public class BowlingGameTest {
+
+    public void roll(int pins) {
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public int score() {
+        throw new RuntimeException("not yet implemented");
+    }
+
+
+}
