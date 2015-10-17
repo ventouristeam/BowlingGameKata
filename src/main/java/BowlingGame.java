06e441f84ec1ed3103b881167d54b809037c7a3c
rRob Swartenbroekx <rob.swartenbroekx@gmail.com>
2015-10-17 12:38:56 +0200

    Implemented gooi en getScore methode op BowlingGame

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,11 +1,39 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class BowlingGame {
 
-    public void roll(int pins) {
-        throw new RuntimeException("not yet implemented");
+    private final static int AANTAL_FRAMES = 10;
+
+    private final List<Frame> unUsedframes;
+    private final List<Frame> usedFrames;
+
+    public BowlingGame() {
+        unUsedframes = new ArrayList<>();
+        usedFrames = new ArrayList<>();
+        fillUnusedFrames();
     }
 
-    public int score() {
-        throw new RuntimeException("not yet implemented");
+    public void gooi(int kegels) {
+        Frame firstUnusedFrame = unUsedframes.iterator().next();
+        firstUnusedFrame.gooi(kegels);
+        if (firstUnusedFrame.getAantalNietGebruikteKansen() == 0) {
+            usedFrames.add(firstUnusedFrame);
+            unUsedframes.remove(firstUnusedFrame);
+        }
+    }
+
+    public int getScore() {
+        return Stream.concat(usedFrames.stream(), unUsedframes.stream()).mapToInt(Frame::getOmgegooideKegels).sum();
+    }
+
+    private void fillUnusedFrames() {
+        for (int i = 0; i < AANTAL_FRAMES; i++) {
+            unUsedframes.add(new Frame());
+        }
     }
 
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,6 +1,50 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingGameTest {
 
+    private BowlingGame bowlingGame;
 
+    @Before
+    public void setUp() {
+        bowlingGame = new BowlingGame();
+    }
 
+    @Test
+    public void gooi0keer_omgegooideKegels_WordenAanScoreToegevoegd() {
+        assertThat(bowlingGame.getScore()).isZero();
+    }
 
+    @Test
+    public void gooi1keer_omgegooideKegels_WordenAanScoreToegevoegd() {
+        bowlingGame.gooi(5);
+
+        assertThat(bowlingGame.getScore()).isEqualTo(5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void gooiInTweeKeerMeerDanTienKegelsOm_IsNietToegelaten() {
+        bowlingGame.gooi(5);
+        bowlingGame.gooi(8);
+
+        assertThat(bowlingGame.getScore()).isEqualTo(19);
+    }
+
+    @Test
+    public void gooi5keer_omgegooideKegels_WordenAanScoreToegevoegd() {
+        bowlingGame.gooi(5);
+        bowlingGame.gooi(2);
+        bowlingGame.gooi(6);
+
+        assertThat(bowlingGame.getScore()).isEqualTo(13);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void gooi_MeerDan10KegelsInEenFrame_IsNietToegelaten() {
+        bowlingGame.gooi(5);
+        bowlingGame.gooi(8);
+    }
 }
