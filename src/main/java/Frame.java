cefcf5c8cb1b908588aca875c65f6193c4fc0cbe
rRob Swartenbroekx <rob.swartenbroekx@gmail.com>
2015-10-17 11:06:40 +0200

    Added Frame

---
+++ src/main/java/Frame.java
@@ -0,0 +1,13 @@
+public class Frame {
+
+    private final static int MAXIMUM_AANTAL_KANSEN = 2;
+    private final static int AANTAL_KEGELS = 10;
+
+    private int gebruikteKansen;
+    private int omgegooideKegels;
+
+    public Frame() {
+        gebruikteKansen = 0;
+        omgegooideKegels = 0;
+    }
+}
