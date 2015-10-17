3dcc0de32d58f8d000e1486a4f50ddbd05f1bfb1
Rob Swartenbroekx <rob.swartenbroekx@gmail.com>
2015-10-17 09:17:32 +0200

    Moved template methods to correct class

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,2 +1,11 @@
 public class BowlingGame {
+
+    public void roll(int pins) {
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public int score() {
+        throw new RuntimeException("not yet implemented");
+    }
+
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,12 +1,6 @@
 public class BowlingGameTest {
 
-    public void roll(int pins) {
-        throw new RuntimeException("not yet implemented");
-    }
 
-    public int score() {
-        throw new RuntimeException("not yet implemented");
-    }
 
 
 }
