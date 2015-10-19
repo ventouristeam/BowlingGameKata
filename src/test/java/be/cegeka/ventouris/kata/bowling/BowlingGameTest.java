6609f1346c6f9a01d582ce0bb8ea099364ac00b2
ventouristeam <ventouristeam@gmail.com>
2015-10-19 13:38:40 +0200

    bktid:bklve: score van volledig spel + refactor tests

--- src/test/java/be/cegeka/ventouris/kata/bowling/BowlingGameTest.java
+++ src/test/java/be/cegeka/ventouris/kata/bowling/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package be.cegeka.ventouris.kata.bowling;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -9,23 +10,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(JUnit4.class)
 public class BowlingGameTest {
 
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
     public void rollZero_ScoreIsZero() {
-        BowlingGame bowlingGame = new BowlingGame();
+        roll(0);
 
-        bowlingGame.roll(0);
-
-        assertThat(bowlingGame.score()).isEqualTo(0);
+        assertScore(0);
     }
 
     @Test
     public void rollFrame_ScoreIsSomVanDe2Rolls() {
-        BowlingGame bowlingGame = new BowlingGame();
+        rollFrame(1, 2);
 
-        bowlingGame.roll(1);
-        bowlingGame.roll(2);
+        assertScore(3);
+    }
 
-        assertThat(bowlingGame.score()).isEqualTo(3);
+    @Test
+    public void rollVolledigSpel_ScoreIsSomVanDeTienFrames() {
+        rollFrame(0, 9);
+        rollFrame(1, 8);
+        rollFrame(2, 7);
+        rollFrame(3, 6);
+        rollFrame(4, 5);
+        rollFrame(5, 4);
+        rollFrame(6, 3);
+        rollFrame(7, 2);
+        rollFrame(8, 1);
+        rollFrame(9, 0);
+
+        assertScore(90);
+    }
+
+    private void roll(int pins) {
+        bowlingGame.roll(pins);
+    }
+
+    private void rollFrame(int roll1, int roll2) {
+        roll(roll1);
+        roll(roll2);
+    }
+
+    private void assertScore(int score) {
+        assertThat(bowlingGame.score()).isEqualTo(score);
     }
 
 }
