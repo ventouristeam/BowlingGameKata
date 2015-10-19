a04dacee6aa9d03d140e32f0bd87aafb8b7db9d2
ventouristeam <ventouristeam>
2015-10-19 13:20:43 +0200

    Game has 10 frames

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
+
+    private List<Frame> frames;
+
+    public BowlingGame() {
+        this.frames = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            frames.add(new Frame());
+        }
+    }
 
     public void roll(int pins) {
         throw new RuntimeException("not yet implemented");
@@ -8,4 +20,7 @@ public class BowlingGame {
         throw new RuntimeException("not yet implemented");
     }
 
+    public List<Frame> getFrames() {
+        return frames;
+    }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,6 +1,12 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BowlingGameTest {
 
-
-
-
+    @Test
+    public void eenGameBestaatUit10Frames() throws Exception {
+        BowlingGame bowlingGame = new BowlingGame();
+        assertThat(bowlingGame.getFrames()).hasSize(10);
+    }
 }
